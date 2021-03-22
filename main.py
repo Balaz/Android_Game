e8bbbfd75adb91d74bfacfc224d5304d7b7ea886
Balazs <majcse@gmail.com>
2021-03-22 23:37:33 +0100

    Set the - changed to the class version - update table method - check table method

--- main.py
+++ main.py
@@ -2,21 +2,145 @@
 
 import pygame
 
-class TicTaToe:
-    def __init__(self):
-        pass
-
-
 # (0,0) = Top Left Corner
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
 GAME_NAME = "TIC TAC TOE MotherFucker"
 
-player = {
-    "x_coord": 50,
-    "y_coord": 50,
-    "step": 20
-}
+
+class TicTacToe:
+    def __init__(self):
+        self.tic_tac_table = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+        ]
+        self.player1 = {
+            "name": "Player 1",
+        }
+        self.player2 = {
+            "name": "Player 2",
+        }
+
+        self.player1_turn = True
+
+    def set_a_mark(self):
+        if self.player1_turn:
+            self.player1_turn = False
+            return "A"
+        else:
+            self.player1_turn = True
+            return "B"
+
+    def update_table(self, tile):
+        self.tic_tac_table[tile[0]][tile[1]] = self.set_a_mark()
+
+    def check_table(self):
+        # First Row
+        if self.tic_tac_table[0][0] == self.tic_tac_table[0][1] == self.tic_tac_table[0][2]:
+            print("The winner is:", self.tic_tac_table[0][0])
+        # Second Row
+        elif self.tic_tac_table[1][0] == self.tic_tac_table[1][1] == self.tic_tac_table[1][2]:
+            print("The winner is:", self.tic_tac_table[1][0])
+        # Third Row
+        elif self.tic_tac_table[2][0] == self.tic_tac_table[2][1] == self.tic_tac_table[2][2]:
+            print("The winner is:", self.tic_tac_table[2][0])
+
+        # First Column
+        elif self.tic_tac_table[0][0] == self.tic_tac_table[1][0] == self.tic_tac_table[2][0]:
+            print("The winner is:", self.tic_tac_table[0][0])
+        # Second Column
+        elif self.tic_tac_table[0][1] == self.tic_tac_table[1][1] == self.tic_tac_table[1][2]:
+            print("The winner is:", self.tic_tac_table[0][1])
+        # Third Column
+        elif self.tic_tac_table[2][0] == self.tic_tac_table[2][1] == self.tic_tac_table[2][2]:
+            print("The winner is:", self.tic_tac_table[2][0])
+
+        # Diagonal 1
+        elif self.tic_tac_table[0][0] == self.tic_tac_table[1][1] == self.tic_tac_table[2][2]:
+            print("The winner is:", self.tic_tac_table[0][0])
+        # Diagonal 2
+        elif self.tic_tac_table[0][2] == self.tic_tac_table[1][1] == self.tic_tac_table[2][0]:
+            print("The winner is:", self.tic_tac_table[0][2])
+
+    def draw_table(self):
+        screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+        pygame.display.set_caption(GAME_NAME)
+
+        table_color = pygame.Color(30, 180, 0)
+        # Vertical
+        pygame.draw.line(screen, table_color, (200, 0), (200, 600), 10)
+        pygame.draw.line(screen, table_color, (400, 0), (400, 600), 10)
+        pygame.draw.line(screen, table_color, (600, 0), (600, 600), 10)
+        pygame.draw.line(screen, table_color, (800, 0), (800, 600), 10)
+
+        # Horizontal
+        pygame.draw.line(screen, table_color, (200, 0), (800, 0), 10)
+        pygame.draw.line(screen, table_color, (200, 200), (800, 200), 10)
+        pygame.draw.line(screen, table_color, (200, 400), (800, 400), 10)
+        pygame.draw.line(screen, table_color, (200, 600), (800, 600), 10)
+
+    def check_events(self, event):
+        pygame.time.delay(100)
+
+        '''
+        # Keyboard Action
+        elif event.type == pygame.KEYDOWN:
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]:  # We can check if a key is pressed like this
+
+            elif keys[pygame.K_RIGHT]:
+
+            elif keys[pygame.K_UP]:
+
+            elif keys[pygame.K_DOWN]:
+
+        '''
+
+        # Mouse Action
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_coord = pygame.mouse.get_pos()
+
+            # First Row
+            if 200 < mouse_coord[0] < 400 and 0 < mouse_coord[1] < 200:
+                self.update_table((0, 0))
+            if 400 < mouse_coord[0] < 600 and 0 < mouse_coord[1] < 200:
+                self.update_table((0, 1))
+            if 600 < mouse_coord[0] < 800 and 0 < mouse_coord[1] < 200:
+                self.update_table((0, 2))
+
+            # Second Row
+            if 200 < mouse_coord[0] < 400 and 200 < mouse_coord[1] < 400:
+                self.update_table((1, 0))
+            if 400 < mouse_coord[0] < 600 and 200 < mouse_coord[1] < 400:
+                self.update_table((1, 1))
+            if 600 < mouse_coord[0] < 800 and 200 < mouse_coord[1] < 400:
+                self.update_table((1, 2))
+
+            # Third Row
+            if 200 < mouse_coord[0] < 400 and 400 < mouse_coord[1] < 600:
+                self.update_table((2, 0))
+            if 400 < mouse_coord[0] < 600 and 400 < mouse_coord[1] < 600:
+                self.update_table((2, 1))
+            if 600 < mouse_coord[0] < 800 and 400 < mouse_coord[1] < 600:
+                self.update_table((2, 2))
+
+            self.check_table()
+            print(game.tic_tac_table)
+
+    def start_game(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+                self.check_events(event)
+                pygame.display.update()
+
+        else:
+            pygame.quit()
+
 
 tile_1 = {
     "x_coord": 200,
@@ -25,68 +149,9 @@ tile_1 = {
     "width": 5
 }
 
-
-def drawTable():
-
-    screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
-    pygame.display.set_caption(GAME_NAME)
-
-    #Red rectangle
-    pygame.draw.rect(screen, (255, 0, 0), (player["x_coord"], player["y_coord"], 50, 50))
-
-    table_color = pygame.Color(30, 180, 0)
-    # Vertical
-    pygame.draw.line(screen, table_color, (200, 0), (200, 600), 10)
-    pygame.draw.line(screen, table_color, (400, 0), (400, 600), 10)
-    pygame.draw.line(screen, table_color, (600, 0), (600, 600), 10)
-    pygame.draw.line(screen, table_color, (800, 0), (800, 600), 10)
-
-    # Horizontal
-    pygame.draw.line(screen, table_color, (200, 0), (800, 0), 10)
-    pygame.draw.line(screen, table_color, (200, 200), (800, 200), 10)
-    pygame.draw.line(screen, table_color, (200, 400), (800, 400), 10)
-    pygame.draw.line(screen, table_color, (200, 600), (800, 600), 10)
-
-def check_event(event):
-    pass
-
-
-def start_game():
-    running = True
-    while running:
-        for event in pygame.event.get():
-            pygame.time.delay(100)
-
-            if event.type == pygame.QUIT:
-                running = False
-
-            # Keyboard Action
-            elif event.type == pygame.KEYDOWN:
-                keys = pygame.key.get_pressed()
-                if keys[pygame.K_LEFT]:  # We can check if a key is pressed like this
-                    player["x_coord"] -= player["step"]
-                elif keys[pygame.K_RIGHT]:
-                    player["x_coord"] += player["step"]
-                elif keys[pygame.K_UP]:
-                    player["y_coord"] -= player["step"]
-                elif keys[pygame.K_DOWN]:
-                    player["y_coord"] += player["step"]
-
-            # Mouse Action
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_coord = pygame.mouse.get_pos()
-                if 200 < mouse_coord[0] < 400 and 0 < mouse_coord[1] < 200:
-                    print(mouse_coord)
-
-            else:
-                pygame.quit()
-            #pygame.display.update()
-
-
 if __name__ == '__main__':
-
     pygame.init()
 
-    drawTable()
-
-    start_game()
+    game = TicTacToe()
+    game.draw_table()
+    game.start_game()
